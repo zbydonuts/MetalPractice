0438ce4e505c1e076d11390ed826c3a933fee231
zhu.bingyi <zhu.bingyi@donuts.ne.jp>
2017-10-07 18:11:29 +0900

    init commit

---
+++ MetalPractice/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MetalPractice
+//
+//  Created by 朱　冰一 on 2017/10/07.
+//  Copyright © 2017年 zhu.bingyi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
